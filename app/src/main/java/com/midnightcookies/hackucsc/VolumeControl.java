2bb1a59eb8ff62c688843347c6e9842beb6e2882
ccanch <ccanch@gmail.com>
2016-01-29 22:13:20 -0800

    editted onHandleIntent()

--- app/src/main/java/com/midnightcookies/hackucsc/VolumeControl.java
+++ app/src/main/java/com/midnightcookies/hackucsc/VolumeControl.java
@@ -3,24 +3,42 @@ package com.midnightcookies.hackucsc;
 import android.app.IntentService;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.provider.MediaStore;
+//import android.app.ActivityManager;
 
 
 public class VolumeControl extends IntentService {
-
 
     public VolumeControl(){
         super("VolumeControlIntentService");
     }
 
     @Override
-    protected void onHandleIntent(Intent workIntent) {
+    protected void onHandleIntent(Intent volumeIntent) {
         /*// Gets data from the incoming Intent
         String dataString = workIntent.getDataString();
         ...
         // Do work here, based on the contents of dataString
         ...*/
+
+        //while( /*volume limiter is enabled*/ ){
+        if (isBluetoothA2dpOn() || AudioManager.isWiredHeadsetOn()) {
+          //while( /*audio exceeds threshold*/ ){
+            //AudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, );
+          //}
+        }
+        //}
     }
 
-
+    /*public boolean isTaskRunning(String thisApp) {
+        ActivityManager activityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> taskInfo = activityManager.getRunningAppProcesses();
+        for(int i = 0; i < taskInfo.size(); i++){
+            if (taskInfo.get(i).processName.equals(thisApp)) {
+                return true;
+            }
+        }
+        return false;
+    }*/
 
 }
