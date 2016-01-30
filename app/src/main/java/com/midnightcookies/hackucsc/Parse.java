6c73e3061140e2be13f38fb72d250293dc3cf8cc
Brian <brraflor@ucsc.edu>
2016-01-30 15:55:32 -0800

    Parse setters and Getter functions

---
+++ app/src/main/java/com/midnightcookies/hackucsc/Parse.java
@@ -0,0 +1,63 @@
+package com.midnightcookies.hackucsc;
+
+import com.parse.ParseClassName;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by Brian on 1/30/16.
+ */
+@ParseClassName("Info")
+public class Parse extends ParseObject {
+
+    private String userName;
+    private String songTitle;
+    private String GeoPoint;
+    private Array Genres;
+
+    Parse(String AuserName, String AsongTitle, String AGeoPoint, Array AGeres){
+        userName = AuserName;
+        songTitle = AsongTitle;
+        GeoPoint = AGeoPoint;
+       //Might Not Work Since We are using an array
+        Genres = AGeres;
+
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public void setSongTitle(String songTitle) {
+        this.songTitle = songTitle;
+    }
+
+    public String getGeoPoint() {
+        return GeoPoint;
+    }
+
+    public void setGeoPoint(String geoPoint) {
+        GeoPoint = geoPoint;
+    }
+
+    public Array getGenres() {
+        return Genres;
+    }
+
+    public void setGenres(Array genres) {
+        Genres = genres;
+    }
+}
